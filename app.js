098235446489d14ef59b63e30add576890b43ac8
JuanCG13 <58788407+JuanCG13@users.noreply.github.com>
2020-12-17 10:28:10 -0300

    commir para subir el juego

---
+++ app.js
@@ -0,0 +1,20 @@
+firebase.initializeApp({
+    apiKey: 'AIzaSyB560MPVjSA38eSrg0IwG8wgrFPeusfpns',
+    authDomain: 'proyectoprofesores-58a82.firebaseapp.com',
+    projectId: 'proyectoprofesores-58a82'
+});
+  
+var db = firebase.firestore();
+
+
+    
+function guardar(){
+
+    var nombre = document.getElementById('usuario').value;
+
+    db.collection("jugadores").add({
+        'nombre':nombre,      
+    })
+
+}
+  
